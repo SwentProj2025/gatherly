262c30bf1439cb6c9a72b61e749a501a3343a767
Gabriel Pineda Serna <gabriel.pinedaserna@epfl.ch>
2025-10-07 01:44:57 +0200

    style(auth): format SignInScreen.kt with ktfmt

--- app/src/main/java/com/android/gatherly/ui/authentication/SignInScreen.kt
+++ app/src/main/java/com/android/gatherly/ui/authentication/SignInScreen.kt
@@ -1,6 +1,5 @@
 package com.android.gatherly.ui.authentication
 
-
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
@@ -9,20 +8,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
-
 @Composable
-fun SignInScreen(
-
-) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Sign In Screen Placeholder")
-    }
-
+fun SignInScreen() {
+  Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    Text("Sign In Screen Placeholder")
+  }
 }
-
 
 @Preview(showBackground = true)
 @Composable
 fun SignInScreenPreview() {
-    SignInScreen()
+  SignInScreen()
 }
