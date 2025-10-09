fa22e5e8fc982378d1b4b875889326a1b33dc75d
gkerjan <gersende.kerjan@epfl.ch>
2025-10-09 11:52:06 +0200

    style(auth): format files

--- app/src/androidTest/java/com/android/gatherly/screen/authentication/SignInViewModelTest.kt
+++ app/src/androidTest/java/com/android/gatherly/screen/authentication/SignInViewModelTest.kt
@@ -8,55 +8,55 @@ import com.android.gatherly.utils.FakeCredentialManager
 import com.android.gatherly.utils.FakeJwtGenerator
 import com.android.gatherly.utils.FirebaseEmulator
 import com.android.gatherly.utils.FirestoreGatherlyTest
+import java.lang.Thread.sleep
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.lang.Thread.sleep
 
 const val WAIT_TIMEOUT = 5_000L
 
 @RunWith(AndroidJUnit4::class)
 class SignInViewModelTest : FirestoreGatherlyTest() {
 
-    private lateinit var signInViewModel: SignInViewModel
+  private lateinit var signInViewModel: SignInViewModel
 
-    // set up then sign out of the firebase
-    @Before
-    override fun setUp() {
-        super.setUp()
-        FirebaseEmulator.auth.signOut()
-    }
+  // set up then sign out of the firebase
+  @Before
+  override fun setUp() {
+    super.setUp()
+    FirebaseEmulator.auth.signOut()
+  }
 
-    @Test
-    fun canSignInWithGoogle() {
-        val fakeGoogleIdToken =
-            FakeJwtGenerator.createFakeGoogleIdToken("12345", email = "test@example.com")
+  @Test
+  fun canSignInWithGoogle() {
+    val fakeGoogleIdToken =
+        FakeJwtGenerator.createFakeGoogleIdToken("12345", email = "test@example.com")
 
-        val fakeCredentialManager = FakeCredentialManager.create(fakeGoogleIdToken)
+    val fakeCredentialManager = FakeCredentialManager.create(fakeGoogleIdToken)
 
-        val context = ApplicationProvider.getApplicationContext<Context>()
+    val context = ApplicationProvider.getApplicationContext<Context>()
 
-        signInViewModel = SignInViewModel()
-        val userSignedIn = signInViewModel.uiState
+    signInViewModel = SignInViewModel()
+    val userSignedIn = signInViewModel.uiState
 
-        signInViewModel.signInWithGoogle(context, fakeCredentialManager)
+    signInViewModel.signInWithGoogle(context, fakeCredentialManager)
 
-        sleep(WAIT_TIMEOUT)
+    sleep(WAIT_TIMEOUT)
 
-        assert(userSignedIn.value)
-        assert(FirebaseEmulator.auth.currentUser != null)
-    }
+    assert(userSignedIn.value)
+    assert(FirebaseEmulator.auth.currentUser != null)
+  }
 
-    @Test
-    fun canSignInAnonymously() {
-        signInViewModel = SignInViewModel()
-        val userSignedIn = signInViewModel.uiState
+  @Test
+  fun canSignInAnonymously() {
+    signInViewModel = SignInViewModel()
+    val userSignedIn = signInViewModel.uiState
 
-        signInViewModel.signInAnonymously()
+    signInViewModel.signInAnonymously()
 
-        sleep(WAIT_TIMEOUT)
+    sleep(WAIT_TIMEOUT)
 
-        assert(userSignedIn.value)
-        assert(FirebaseEmulator.auth.currentUser != null)
-    }
+    assert(userSignedIn.value)
+    assert(FirebaseEmulator.auth.currentUser != null)
+  }
 }
