8c4eec2f915ae96c8e56d132d15c75888a000531
clairechaffard <claire.chaffard@epfl.ch>
2025-10-13 13:31:11 +0200

    feat(ui): create a new branch for events overview
    Created a new file where we will implement the overview events screen

---
+++ app/src/main/java/com/android/gatherly/ui/events/EventsOverviewScreen.kt
@@ -0,0 +1,11 @@
+package com.android.gatherly.ui.events
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EventsOverviewScreen(viewModel: EventsViewModel = viewModel()) {
+
+}
