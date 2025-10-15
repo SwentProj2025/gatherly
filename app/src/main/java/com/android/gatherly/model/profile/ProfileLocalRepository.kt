326c1aee07ea60bfc896da59c416a7b21a88e6d7
gkerjan <gersende.kerjan@epfl.ch>
2025-10-15 22:14:00 +0200

    style(profile): run ktfmtFormat

--- app/src/main/java/com/android/gatherly/model/profile/ProfileLocalRepository.kt
+++ app/src/main/java/com/android/gatherly/model/profile/ProfileLocalRepository.kt
@@ -1,36 +1,35 @@
 package com.android.gatherly.model.profile
 
-
 class ProfileLocalRepository : ProfileRepository {
 
-    private val profiles: MutableList<Profile> = mutableListOf()
+  private val profiles: MutableList<Profile> = mutableListOf()
 
-    override suspend fun getProfileByUid(uid: String): Profile? {
-        return profiles.find { it.uid == uid }
-    }
+  override suspend fun getProfileByUid(uid: String): Profile? {
+    return profiles.find { it.uid == uid }
+  }
 
-    override suspend fun addProfile(profile: Profile) {
-        profiles += profile
-    }
+  override suspend fun addProfile(profile: Profile) {
+    profiles += profile
+  }
 
-    override suspend fun updateProfile(profile: Profile) {
-        val index = profiles.indexOf(profile)
-        if (index == -1) {
-            throw IllegalArgumentException()
-        } else {
-            profiles[index] = profile
-        }
+  override suspend fun updateProfile(profile: Profile) {
+    val index = profiles.indexOf(profile)
+    if (index == -1) {
+      throw IllegalArgumentException()
+    } else {
+      profiles[index] = profile
     }
+  }
 
-    override suspend fun deleteProfile(uid: String) {
-        profiles.filter { it.uid != uid }
-    }
+  override suspend fun deleteProfile(uid: String) {
+    profiles.filter { it.uid != uid }
+  }
 
-    override suspend fun isUidRegistered(uid: String): Boolean {
-        return profiles.find { it.uid == uid } != null
-    }
+  override suspend fun isUidRegistered(uid: String): Boolean {
+    return profiles.find { it.uid == uid } != null
+  }
 
-    override suspend fun findProfilesByUidSubstring(uidSubstring: String): List<Profile> {
-        return profiles.filter { it.name.contains(uidSubstring, ignoreCase = true) }
-    }
+  override suspend fun findProfilesByUidSubstring(uidSubstring: String): List<Profile> {
+    return profiles.filter { it.name.contains(uidSubstring, ignoreCase = true) }
+  }
 }
