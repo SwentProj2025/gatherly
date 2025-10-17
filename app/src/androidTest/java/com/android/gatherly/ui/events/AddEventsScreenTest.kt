8c683a3b29c228858a2ed6671f2e643f0f941276
coco-jrdn <jourdan.colombe@gmail.com>
2025-10-17 02:26:53 +0200

    feat(events): Implement AddEventScreen with tests, strings and fix for the view model

---
+++ app/src/androidTest/java/com/android/gatherly/ui/events/AddEventsScreenTest.kt
@@ -0,0 +1,160 @@
+package com.android.gatherly.ui.events
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.isDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performTextInput
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.gatherly.model.event.Event
+import com.android.gatherly.model.event.EventStatus
+import com.android.gatherly.model.event.EventsLocalRepository
+import com.android.gatherly.model.event.EventsRepository
+import com.android.gatherly.model.profile.Profile
+import com.android.gatherly.model.profile.ProfileLocalRepository
+import com.android.gatherly.model.profile.ProfileRepository
+import com.android.gatherly.ui.todo.AddToDoScreenTestTags
+import com.android.gatherly.utils.InMemoryGatherlyTest
+import com.google.firebase.Timestamp
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/** Tests for the AddEventScreen */
+@OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(AndroidJUnit4::class)
+class AddEventsScreenTest : InMemoryGatherlyTest() {
+  @get:Rule val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+  // declare viewModel and repositories
+  private lateinit var addEventsViewModel: AddEventViewModel
+  private lateinit var eventsRepository: EventsRepository
+  private lateinit var profileRepository: ProfileRepository
+
+  @Before
+  override fun setUp() {
+    super.setUp()
+    // initialize repos and viewModel
+    profileRepository = ProfileLocalRepository()
+    eventsRepository = EventsLocalRepository()
+    addEventsViewModel = AddEventViewModel(profileRepository, eventsRepository)
+
+    // fill the profile and events repositories with profiles and event
+    runBlocking { fill_repositories() }
+
+    composeTestRule.setContent { AddEventScreen(addEventsViewModel) }
+  }
+
+  /*----------------------------------------Profiles--------------------------------------------*/
+  val profile1: Profile =
+      Profile(
+          uid = "1",
+          name = "Profile1",
+          focusSessionIds = emptyList(),
+          eventIds = emptyList(),
+          groupIds = emptyList(),
+          friendUids = emptyList())
+
+  val profile2: Profile =
+      Profile(
+          uid = "2",
+          name = "Profile2",
+          focusSessionIds = emptyList(),
+          eventIds = emptyList(),
+          groupIds = emptyList(),
+          friendUids = emptyList())
+
+  val profile3: Profile =
+      Profile(
+          uid = "3",
+          name = "Profile3",
+          focusSessionIds = emptyList(),
+          eventIds = emptyList(),
+          groupIds = emptyList(),
+          friendUids = emptyList())
+
+  val participantProfile: Profile =
+      Profile(
+          uid = "4",
+          name = "Participant",
+          focusSessionIds = emptyList(),
+          eventIds = emptyList(),
+          groupIds = emptyList(),
+          friendUids = emptyList())
+
+  val ownerProfile: Profile =
+      Profile(
+          uid = "0",
+          name = "Owner",
+          focusSessionIds = emptyList(),
+          eventIds = emptyList(),
+          groupIds = emptyList(),
+          friendUids = emptyList())
+
+  /*----------------------------------------Event-----------------------------------------------*/
+  val event: Event =
+      Event(
+          id = "0",
+          title = "Event title",
+          description = "Describing this great event",
+          creatorName = "my name :)",
+          location = null,
+          date = Timestamp.now(),
+          startTime = Timestamp.now(),
+          endTime = Timestamp.now(),
+          creatorId = ownerProfile.uid,
+          participants = listOf(ownerProfile.uid, participantProfile.uid),
+          status = EventStatus.UPCOMING)
+
+  /** Check that all components are displayed */
+  @Test
+  fun displayAllComponents() {
+    composeTestRule.onNodeWithTag(AddEventScreenTestTags.INPUT_NAME).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(AddEventScreenTestTags.INPUT_DESCRIPTION).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(AddEventScreenTestTags.INPUT_CREATOR).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(AddEventScreenTestTags.INPUT_PARTICIPANT).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(AddEventScreenTestTags.INPUT_LOCATION).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(AddEventScreenTestTags.INPUT_DATE).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(AddEventScreenTestTags.INPUT_START).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(AddEventScreenTestTags.INPUT_END).assertIsDisplayed()
+    composeTestRule
+        .onNodeWithTag(AddToDoScreenTestTags.ERROR_MESSAGE, useUnmergedTree = true)
+        .assertIsNotDisplayed()
+    composeTestRule.onNodeWithTag(AddEventScreenTestTags.BTN_SAVE)
+  }
+
+  /** Check that menus are displayed */
+  @Test
+  fun displayMenus() {
+    composeTestRule.onNodeWithTag(AddEventScreenTestTags.INPUT_LOCATION).performTextInput("Paris")
+    composeTestRule.waitUntil(timeoutMillis = 5000L) {
+      composeTestRule.onNodeWithTag(AddEventScreenTestTags.LOCATION_MENU).isDisplayed()
+    }
+
+    composeTestRule
+        .onNodeWithTag(AddEventScreenTestTags.INPUT_PARTICIPANT)
+        .performTextInput("Participant")
+    composeTestRule.waitUntil(timeoutMillis = 5000L) {
+      composeTestRule.onNodeWithTag(AddEventScreenTestTags.PARTICIPANT_MENU).isDisplayed()
+    }
+  }
+
+  // This function fills the profile repository with the created profiles, and the event repository
+  // with the created event
+  fun fill_repositories() {
+    runTest {
+      profileRepository.addProfile(profile1)
+      profileRepository.addProfile(profile2)
+      profileRepository.addProfile(profile3)
+      profileRepository.addProfile(participantProfile)
+      profileRepository.addProfile(ownerProfile)
+      eventsRepository.addEvent(event)
+    }
+  }
+}
