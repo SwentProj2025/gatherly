f25c48f17d73241fd7e01cfe0b382ff2fc7645fa
gkerjan <gersende.kerjan@epfl.ch>
2025-10-26 19:13:21 +0100

    feat(homepage): implement a placeholder viewmodel
    Add the agreed upon elements to the homepage viewmodel with UI State
    values and functions

--- app/src/main/java/com/android/gatherly/ui/homePage/HomePageViewModel.kt
+++ app/src/main/java/com/android/gatherly/ui/homePage/HomePageViewModel.kt
@@ -4,19 +4,54 @@ import androidx.credentials.ClearCredentialStateRequest
 import androidx.credentials.CredentialManager
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.android.gatherly.model.event.Event
+import com.android.gatherly.model.event.EventsRepository
+import com.android.gatherly.model.event.EventsRepositoryFirestore
+import com.android.gatherly.model.map.DisplayedMapElement
+import com.android.gatherly.model.profile.Profile
+import com.android.gatherly.model.profile.ProfileRepository
+import com.android.gatherly.model.profile.ProfileRepositoryFirestore
+import com.android.gatherly.model.todo.ToDo
+import com.android.gatherly.model.todo.ToDosRepository
+import com.android.gatherly.model.todo.ToDosRepositoryFirestore
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-data class HomePageUIState(val errorMsg: String? = null, val signedOut: Boolean = false)
+data class HomePageUIState(
+    val displayableTodos: List<ToDo> = emptyList(),
+    val displayableEvents: List<Event> = emptyList(),
+    val expandedElement: DisplayedMapElement? = null,
+    val friends: List<Profile> = emptyList(),
+    val todos: List<ToDo> = emptyList(),
+    val timerString: String = "",
+    val errorMsg: String? = null,
+    val signedOut: Boolean = false
+)
 
-class HomePageViewModel() : ViewModel() {
+class HomePageViewModel(
+    private val eventsRepository: EventsRepository = EventsRepositoryFirestore(Firebase.firestore),
+    private val toDosRepository: ToDosRepository = ToDosRepositoryFirestore(Firebase.firestore),
+    private val profileRepository: ProfileRepository =
+        ProfileRepositoryFirestore(Firebase.firestore)
+) : ViewModel() {
 
   private val _uiState = MutableStateFlow(HomePageUIState())
   val uiState: StateFlow<HomePageUIState> = _uiState.asStateFlow()
+
+  /**
+   * Handles a tap on a displayable element so that it can be expanded
+   *
+   * @param displayable the item we want to expand
+   */
+  fun selectItem(displayable: DisplayedMapElement) {}
+
+  /** Handles the dismissal of an expanded element */
+  fun dismissItem() {}
 
   /** Initiates sign-out */
   fun signOut(credentialManager: CredentialManager): Unit {
