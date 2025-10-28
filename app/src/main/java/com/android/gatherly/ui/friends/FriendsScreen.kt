8b193b64191db060724c284f0d9273fccb2b0d9b
clairechaffard <claire.chaffard@epfl.ch>
2025-10-28 14:02:39 +0100

    feat(ui): first version friends' screen
    Implemented a first version of the friends screen ui, with button unfollow in card friend item

--- app/src/main/java/com/android/gatherly/ui/friends/FriendsScreen.kt
+++ app/src/main/java/com/android/gatherly/ui/friends/FriendsScreen.kt
@@ -1,13 +1,35 @@
 package com.android.gatherly.ui.friends
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults.buttonColors
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.credentials.CredentialManager
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.android.gatherly.ui.navigation.NavigationTestTags
@@ -23,7 +45,36 @@ fun FriendsScreen(
     friendsViewModel: FriendsViewModel = viewModel(),
     credentialManager: CredentialManager = CredentialManager.create(LocalContext.current),
     goBack: () -> Unit,
+    findNewFriend: () -> Unit,
 ) {
+
+    /* TODO : to add in the arguments of the FriendsScreen function
+
+        friendsViewModel: FriendsViewModel =
+        viewModel(
+            factory =
+                GenericViewModelFactory<FriendsViewModel> {
+                  FriendsViewModel(
+                      ProfileRepositoryFirestore(Firebase.firestore),
+                      currentUserId = Firebase.auth.currentUser?.uid ?: "")
+                }),
+
+
+     */
+
+    /*
+
+    val currentUserIdFromVM = friendsViewModel.currentUserId
+
+    val uiState by friendsViewModel.uiState.collectAsState()
+    val friendslist = uiState.friends
+
+    LaunchedEffect(currentUserIdFromVM) {
+        if (currentUserIdFromVM.isNotBlank()) {
+            friendsViewModel.refreshFriends(currentUserIdFromVM)
+        }
+    }
+
 
   Scaffold(
       topBar = {
@@ -33,8 +84,105 @@ fun FriendsScreen(
             goBack = goBack)
       },
       content = { padding ->
-        Text(
-            text = "Friends accounts page",
-            modifier = Modifier.padding(padding).testTag(FriendsScreenTestTags.FriendsText))
+          LazyColumn(
+              contentPadding = PaddingValues(vertical = 8.dp),
+              modifier =
+                  Modifier.fillMaxWidth()
+                      .padding(horizontal = 16.dp)
+                      .padding(padding)) {
+
+              if (friendslist.isEmpty()) {
+                  item {
+                      Text(
+                          text = "You have no friends added yet.",  // todo : place it to R.string
+                          modifier = Modifier
+                              .padding(16.dp))
+                  }
+              } else {
+                  items(friendslist.size) { index ->
+                      val friend : String = friendslist[index] ?: throw Exception("Friend at index $index is null")
+                      FriendItem(friend = friend,
+                          unfollow = { friendsViewModel.unfollowFriend(currentUserIdFromVM, friend ?: "") } )
+
+                  }
+              }
+
+              item {
+                  Button(
+                      onClick = { findNewFriend() },
+                      modifier =
+                          Modifier.fillMaxWidth()
+                              .height(80.dp)
+                              .padding(vertical = 12.dp),
+                      shape = RoundedCornerShape(12.dp),
+                      colors = buttonColors(containerColor = Color(0xFF9ADCE5))) {
+
+                      Text(
+                          text = "Find New Friends", // todo : stringResource(R.string.create_event_button_title)
+                          fontSize = 16.sp,
+                          fontWeight = FontWeight.Medium,
+                          color = MaterialTheme.colorScheme.onPrimary)
+                  }
+              }
+
+          }
+
       })
+}
+
+@Composable
+fun FriendItem(friend: String, unfollow: () -> Unit) {
+    Card(
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.onSurface,
+            contentColor = MaterialTheme.colorScheme.primary),
+        modifier =
+            Modifier.clickable(onClick = unfollow)
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)) {
+            //todo first column photo
+            // todo second column is a row : first row username second row bio
+
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+
+        Image(
+            painter = friend.profilePicture,
+                ?: rememberAsyncImagePainter(R.drawable.ic_default_profile_picture),
+            contentDescription = "Profile picture of ${friend.surname}",
+            modifier = Modifier
+                .size(56.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = friend.surname ?: "Friend Name",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.Medium)
+                Text(
+                    text = friend.status ?: "Friend Status",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.primary,)
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Button(
+            onClick = unfollow,
+            modifier = Modifier.wrapContentWidth()
+        ) {
+            Text("Unfollow")
+        }
+        }
+    }
+}
+
+
+*/
 }
