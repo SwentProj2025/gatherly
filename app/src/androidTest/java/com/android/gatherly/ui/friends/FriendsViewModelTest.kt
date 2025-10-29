1d8fd14d4071ac3491c6bb3bb4644f4de2e9ccec
clairechaffard <claire.chaffard@epfl.ch>
2025-10-29 03:00:57 +0100

    fix: ktfmtFormat

--- app/src/androidTest/java/com/android/gatherly/ui/friends/FriendsViewModelTest.kt
+++ app/src/androidTest/java/com/android/gatherly/ui/friends/FriendsViewModelTest.kt
@@ -26,117 +26,114 @@ private const val DELAY = 200L
 @OptIn(ExperimentalCoroutinesApi::class)
 class FriendsViewModelTest {
 
-    private lateinit var repository: ProfileLocalRepository
-    private lateinit var viewModel: FriendsViewModel
+  private lateinit var repository: ProfileLocalRepository
+  private lateinit var viewModel: FriendsViewModel
 
-    private val userA: Profile =
-        Profile(
-            uid = "A",
-            username = "alice",
-            name = "ProfileA",
-            focusSessionIds = emptyList(),
-            eventIds = emptyList(),
-            groupIds = emptyList(),
-            friendUids = listOf("B"),
-            profilePicture = "profileA.png"
-        )
+  private val userA: Profile =
+      Profile(
+          uid = "A",
+          username = "alice",
+          name = "ProfileA",
+          focusSessionIds = emptyList(),
+          eventIds = emptyList(),
+          groupIds = emptyList(),
+          friendUids = listOf("B"),
+          profilePicture = "profileA.png")
 
-    private val userB: Profile =
-        Profile(
-            uid = "B",
-            username = "bob",
-            name = "ProfileB",
-            focusSessionIds = emptyList(),
-            eventIds = emptyList(),
-            groupIds = emptyList(),
-            friendUids = listOf("A"),
-            profilePicture = "profileB.png"
-        )
+  private val userB: Profile =
+      Profile(
+          uid = "B",
+          username = "bob",
+          name = "ProfileB",
+          focusSessionIds = emptyList(),
+          eventIds = emptyList(),
+          groupIds = emptyList(),
+          friendUids = listOf("A"),
+          profilePicture = "profileB.png")
 
-    val userC: Profile =
-        Profile(
-            uid = "C",
-            username = "charlie",
-            name = "ProfileC",
-            focusSessionIds = emptyList(),
-            eventIds = emptyList(),
-            groupIds = emptyList(),
-            friendUids = emptyList(),
-            profilePicture = "profileC.png"
-        )
+  val userC: Profile =
+      Profile(
+          uid = "C",
+          username = "charlie",
+          name = "ProfileC",
+          focusSessionIds = emptyList(),
+          eventIds = emptyList(),
+          groupIds = emptyList(),
+          friendUids = emptyList(),
+          profilePicture = "profileC.png")
 
-    @Before
-    fun setup() = runTest {
-        repository = ProfileLocalRepository()
-        repository.addProfile(userA)
-        repository.addProfile(userB)
-        repository.addProfile(userC)
+  @Before
+  fun setup() = runTest {
+    repository = ProfileLocalRepository()
+    repository.addProfile(userA)
+    repository.addProfile(userB)
+    repository.addProfile(userC)
 
-        viewModel = FriendsViewModel(repository, "A")
-    }
+    viewModel = FriendsViewModel(repository, "A")
+  }
 
-    @Test
-    fun testInitialStateShouldContainEachFriends() = runTest {
-        val state = viewModel.uiState.value
-        assertEquals(listOf("B"), state.friends)
-        assertTrue(state.listNoFriends.contains("charlie"))
-    }
+  @Test
+  fun testInitialStateShouldContainEachFriends() = runTest {
+    val state = viewModel.uiState.value
+    assertEquals(listOf("B"), state.friends)
+    assertTrue(state.listNoFriends.contains("charlie"))
+  }
 
-    @Test
-    fun testFollowFriendCFromProfileA() = runTest {
-        viewModel.followFriend("C", "A")
+  @Test
+  fun testFollowFriendCFromProfileA() = runTest {
+    viewModel.followFriend("C", "A")
 
-        // Wait until the friends list is updated
-        withContext(Dispatchers.Default.limitedParallelism(1)) {
-            withTimeout(TIMEOUT) {
-                while (viewModel.uiState.value.friends != listOf("B", "C")) {
-                    delay(DELAY)
-                }
-            }
+    // Wait until the friends list is updated
+    withContext(Dispatchers.Default.limitedParallelism(1)) {
+      withTimeout(TIMEOUT) {
+        while (viewModel.uiState.value.friends != listOf("B", "C")) {
+          delay(DELAY)
         }
-
-        val state = viewModel.uiState.value
-        assertTrue(state.friends.contains("C"))
+      }
     }
 
-    @Test
-    fun testUnfollowFriendBFromProfileA() = runTest {
-        viewModel.unfollowFriend("B", "A")
+    val state = viewModel.uiState.value
+    assertTrue(state.friends.contains("C"))
+  }
 
-        // Wait until the friends list is updated
-        withContext(Dispatchers.Default.limitedParallelism(1)) {
-            withTimeout(TIMEOUT) {
-                while (viewModel.uiState.value.friends == listOf("C")) {
-                    delay(DELAY)
-                }
-            }
+  @Test
+  fun testUnfollowFriendBFromProfileA() = runTest {
+    viewModel.unfollowFriend("B", "A")
+
+    // Wait until the friends list is updated
+    withContext(Dispatchers.Default.limitedParallelism(1)) {
+      withTimeout(TIMEOUT) {
+        while (viewModel.uiState.value.friends == listOf("C")) {
+          delay(DELAY)
         }
-
-        val state = viewModel.uiState.value
-        assertFalse(state.friends.contains("B"))
+      }
     }
 
-    @Test
-    fun testGetProfilePictureOfFriend() = runTest {
-        val state = viewModel.uiState.value
-        state.friends.forEach { friend ->
-            val profilePicture = viewModel.getFriendProfilePicture(friend)
+    val state = viewModel.uiState.value
+    assertFalse(state.friends.contains("B"))
+  }
 
-            withContext(Dispatchers.Default.limitedParallelism(1)) {
-                withTimeout(TIMEOUT) {
-                    var profile: String? = viewModel.getFriendProfilePicture(friend)
-                    while (profile != "profileB.png") {
-                        profile = viewModel.getFriendProfilePicture(friend)
-                        delay(DELAY)
-                    }
-                }
-            }
+  @Test
+  fun testGetProfilePictureOfFriend() = runTest {
+    val state = viewModel.uiState.value
+    state.friends.forEach { friend ->
+      val profilePicture = viewModel.getFriendProfilePicture(friend)
 
-            if (friend == "B") {
-                assertEquals("profileB.png", profilePicture)
-            } else {
-                fail("Unexpected friend username: $friend")
-            }
+      withContext(Dispatchers.Default.limitedParallelism(1)) {
+        withTimeout(TIMEOUT) {
+          var profile: String? = viewModel.getFriendProfilePicture(friend)
+          while (profile != "profileB.png") {
+            profile = viewModel.getFriendProfilePicture(friend)
+            delay(DELAY)
+          }
         }
+      }
+
+      if (friend == "B") {
+        assertEquals("profileB.png", profilePicture)
+      } else {
+        fail("Unexpected friend username: $friend")
+      }
     }
+  }
 }
