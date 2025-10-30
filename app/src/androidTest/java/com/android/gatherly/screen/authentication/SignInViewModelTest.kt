394d347e6c89f898ade933714b9b89abea48b912
claumnl <116760695+claumnl@users.noreply.github.com>
2025-10-30 15:49:38 +0100

    test(auth): extend SignInViewModelTest to test destination handling

--- app/src/androidTest/java/com/android/gatherly/screen/authentication/SignInViewModelTest.kt
+++ app/src/androidTest/java/com/android/gatherly/screen/authentication/SignInViewModelTest.kt
@@ -3,31 +3,23 @@ package com.android.gatherly.screen.authentication
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.android.gatherly.model.profile.ProfileLocalRepository
-import com.android.gatherly.model.profile.ProfileRepository
 import com.android.gatherly.ui.authentication.SignInViewModel
 import com.android.gatherly.utils.FakeCredentialManager
 import com.android.gatherly.utils.FakeJwtGenerator
 import com.android.gatherly.utils.FirebaseEmulator
 import com.android.gatherly.utils.FirestoreGatherlyTest
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.withContext
-import kotlinx.coroutines.withTimeout
+import java.lang.Thread.sleep
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 const val WAIT_TIMEOUT = 5_000L
 
-@OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(AndroidJUnit4::class)
 class SignInViewModelTest : FirestoreGatherlyTest() {
 
   private lateinit var signInViewModel: SignInViewModel
-  private val profileRepository: ProfileRepository = ProfileLocalRepository()
 
   // set up then sign out of the firebase
   @Before
@@ -37,7 +29,7 @@ class SignInViewModelTest : FirestoreGatherlyTest() {
   }
 
   @Test
-  fun canSignInWithGoogle() = runTest {
+  fun canSignInWithGoogle() {
     val fakeGoogleIdToken =
         FakeJwtGenerator.createFakeGoogleIdToken("12345", email = "test@example.com")
 
@@ -45,41 +37,54 @@ class SignInViewModelTest : FirestoreGatherlyTest() {
 
     val context = ApplicationProvider.getApplicationContext<Context>()
 
-    signInViewModel = SignInViewModel(profileRepository = profileRepository)
+    signInViewModel = SignInViewModel()
     val userSignedIn = signInViewModel.uiState
 
     signInViewModel.signInWithGoogle(context, fakeCredentialManager)
 
-    withContext(Dispatchers.Default.limitedParallelism(1)) {
-      withTimeout(5000L) {
-        while (!signInViewModel.uiState.value) {
-          delay(50)
-        }
-      }
-    }
+    sleep(WAIT_TIMEOUT)
 
     assert(userSignedIn.value)
     assert(FirebaseEmulator.auth.currentUser != null)
-    assert(!profileRepository.initProfileIfMissing(FirebaseEmulator.auth.currentUser?.uid!!, ""))
   }
 
   @Test
-  fun canSignInAnonymously() = runTest {
-    signInViewModel = SignInViewModel(profileRepository = profileRepository)
+  fun canSignInAnonymously() {
+    signInViewModel = SignInViewModel()
     val userSignedIn = signInViewModel.uiState
 
     signInViewModel.signInAnonymously()
 
-    withContext(Dispatchers.Default.limitedParallelism(1)) {
-      withTimeout(5000L) {
-        while (!signInViewModel.uiState.value) {
-          delay(50)
-        }
-      }
-    }
+    sleep(WAIT_TIMEOUT)
 
     assert(userSignedIn.value)
     assert(FirebaseEmulator.auth.currentUser != null)
-    assert(!profileRepository.initProfileIfMissing(FirebaseEmulator.auth.currentUser?.uid!!, ""))
   }
+
+  @Test
+  fun googleSignIn_setsDestinationToInitProfile_whenNoUsername() {
+    val fakeGoogleIdToken =
+      FakeJwtGenerator.createFakeGoogleIdToken("uid_123", email = "test@example.com")
+    val fakeCredentialManager = FakeCredentialManager.create(fakeGoogleIdToken)
+    val context = ApplicationProvider.getApplicationContext<Context>()
+
+    signInViewModel = SignInViewModel()
+    signInViewModel.signInWithGoogle(context, fakeCredentialManager)
+
+    sleep(WAIT_TIMEOUT)
+
+    assert(signInViewModel.destination.value == "init_profile")
+  }
+
+  @Test
+  fun anonymousSignIn_setsDestinationToInitProfile_whenNoUsername() {
+    signInViewModel = SignInViewModel()
+    signInViewModel.signInAnonymously()
+
+    sleep(WAIT_TIMEOUT)
+
+    assert(signInViewModel.destination.value == "init_profile")
+  }
+
+
 }
