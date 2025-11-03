7cdc0d138adb44ee5d10e70c08f5574df5abfacc
gkerjan <gersende.kerjan@epfl.ch>
2025-11-03 22:59:19 +0100

    fix: refactor MapScreen test to not depend on Firestore

--- app/src/androidTest/java/com/android/gatherly/ui/map/MapScreenTest.kt
+++ app/src/androidTest/java/com/android/gatherly/ui/map/MapScreenTest.kt
@@ -2,20 +2,31 @@ package com.android.gatherly.ui.map
 
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
-import com.android.gatherly.utils.FirestoreGatherlyTest
+import com.android.gatherly.model.event.EventsLocalRepository
+import com.android.gatherly.model.event.EventsRepository
+import com.android.gatherly.model.todo.ToDosRepository
+import com.android.gatherly.model.todo.ToDosRepositoryLocalMapTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 /** Tests for the MapScreen composable. */
-class MapScreenTest : FirestoreGatherlyTest() {
+class MapScreenTest {
 
   @get:Rule val composeTestRule = createComposeRule()
 
+  private lateinit var toDosRepository: ToDosRepository
+  private lateinit var eventsRepository: EventsRepository
+  private lateinit var mapViewModel: MapViewModel
+
   @Before
-  override fun setUp() {
-    super.setUp()
-    composeTestRule.setContent { MapScreen() }
+  fun setUp() {
+    toDosRepository = ToDosRepositoryLocalMapTest()
+    eventsRepository = EventsLocalRepository()
+    mapViewModel =
+        MapViewModel(todosRepository = toDosRepository, eventsRepository = eventsRepository)
+
+    composeTestRule.setContent { MapScreen(mapViewModel) }
   }
 
   /** Checks if google map is displayed */
