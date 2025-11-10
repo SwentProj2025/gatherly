b8f90cf8fed6ec65d3a287000e4e915dbb877eed
claumnl <116760695+claumnl@users.noreply.github.com>
2025-11-10 16:00:08 +0100

    fix(profile): add the Firebase.storage argument to the ProfileRepositoryFirestore in ProfileRepositoryProvider

--- app/src/main/java/com/android/gatherly/model/profile/ProfileRepositoryProvider.kt
+++ app/src/main/java/com/android/gatherly/model/profile/ProfileRepositoryProvider.kt
@@ -2,6 +2,7 @@ package com.android.gatherly.model.profile
 
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 /**
  * Provides a single instance of the repository in the app. `repository` is mutable for testing
@@ -10,7 +11,7 @@ import com.google.firebase.ktx.Firebase
 object ProfileRepositoryProvider {
 
   private val _repository: ProfileRepository by lazy {
-    ProfileRepositoryFirestore(Firebase.firestore)
+    ProfileRepositoryFirestore(Firebase.firestore, Firebase.storage)
   }
 
   var repository: ProfileRepository = _repository
