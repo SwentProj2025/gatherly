16ca49f6f454e512bd97659b79fdbbde08c358d5
gkerjan <gersende.kerjan@epfl.ch>
2025-11-17 15:37:36 +0100

    test(auth): adapt and add tests for SignInVM

--- app/src/androidTest/java/com/android/gatherly/ui/authentication/SignInViewModelTest.kt
+++ app/src/androidTest/java/com/android/gatherly/ui/authentication/SignInViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.android.gatherly.ui.authentication
 
 import android.content.Context
+import androidx.credentials.CredentialManager
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.gatherly.model.profile.ProfileLocalRepository
@@ -36,6 +37,7 @@ class SignInViewModelTest : FirestoreGatherlyTest() {
     signInViewModel = SignInViewModel(profileRepository)
   }
 
+  /** Sign in with google is done correctly, and then clearing the UI State works as well */
   @Test
   fun canSignInWithGoogle_destinationInitProfileIfFirst() = runTest {
     val fakeGoogleIdToken =
@@ -46,10 +48,13 @@ class SignInViewModelTest : FirestoreGatherlyTest() {
     val context = ApplicationProvider.getApplicationContext<Context>()
 
     signInViewModel = SignInViewModel(profileRepository = profileRepository)
-    val userSignedIn = signInViewModel.uiState
 
     signInViewModel.signInWithGoogle(context, fakeCredentialManager)
 
+    // Check that the screen is loading
+    assert(signInViewModel.uiState.isLoading)
+
+    // Wait for the user to be signed in
     withContext(Dispatchers.Default.limitedParallelism(1)) {
       withTimeout(5000L) {
         while (!signInViewModel.uiState.signedIn) {
@@ -58,16 +63,59 @@ class SignInViewModelTest : FirestoreGatherlyTest() {
       }
     }
 
-    assert(userSignedIn.signedIn)
+    // Check the loading, firebase user, destination screen and signedIn
+    assert(!signInViewModel.uiState.isLoading)
     assert(FirebaseEmulator.auth.currentUser != null)
     assert(!profileRepository.initProfileIfMissing(FirebaseEmulator.auth.currentUser?.uid!!, ""))
     assert(signInViewModel.uiState.destinationScreen == "init_profile")
+    assert(signInViewModel.uiState.signedIn)
+
+    // Reset the UI state
+    signInViewModel.resetAuth()
+
+    // Check that it is correctly cleared
+    assert(!signInViewModel.uiState.isLoading)
+    assert(!signInViewModel.uiState.signedIn)
+    assert(signInViewModel.uiState.destinationScreen == null)
+    assert(signInViewModel.uiState.errorMessage == null)
+  }
+
+  /**
+   * Checks that signing into google with no credentials sets the error message and that clearing it
+   * works
+   */
+  @Test
+  fun signInWithNoGoogleErrorMessage() = runTest {
+    // Sign in with no creds
+    val context = ApplicationProvider.getApplicationContext<Context>()
+    signInViewModel = SignInViewModel(profileRepository = profileRepository)
+    signInViewModel.signInWithGoogle(context, CredentialManager.create(context))
+
+    // Check that the screen is loading
+    assert(signInViewModel.uiState.isLoading)
+
+    // Wait for the error message to appear
+    withContext(Dispatchers.Default.limitedParallelism(1)) {
+      withTimeout(5000L) {
+        while (signInViewModel.uiState.errorMessage == null) {
+          delay(50)
+        }
+      }
+    }
+
+    // Check that there is an error message ans the screen is no longer loading
+    assert(signInViewModel.uiState.errorMessage != null)
+    assert(!signInViewModel.uiState.isLoading)
+
+    // Clear error message and check that it is cleared
+    signInViewModel.resetErrorMessage()
+
+    assert(signInViewModel.uiState.errorMessage == null)
   }
 
   @Test
   fun canSignInAnonymously_destinationInitProfileIfFirst() = runTest {
     signInViewModel = SignInViewModel(profileRepository = profileRepository)
-    val userSignedIn = signInViewModel.uiState
 
     signInViewModel.signInAnonymously()
 
@@ -79,7 +127,7 @@ class SignInViewModelTest : FirestoreGatherlyTest() {
       }
     }
 
-    assert(userSignedIn.signedIn)
+    assert(signInViewModel.uiState.signedIn)
     assert(FirebaseEmulator.auth.currentUser != null)
     assert(!profileRepository.initProfileIfMissing(FirebaseEmulator.auth.currentUser?.uid!!, ""))
     assert(signInViewModel.uiState.destinationScreen == "init_profile")
