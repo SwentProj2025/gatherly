5be08b39a4f1b70aab40cec8f4340de128834d4f
coco-jrdn <jourdan.colombe@gmail.com>
2025-11-21 00:43:15 +0100

    feat(badge): start BadgeVM implementation

--- app/src/main/java/com/android/gatherly/ui/badge/BadgeViewModel.kt
+++ app/src/main/java/com/android/gatherly/ui/badge/BadgeViewModel.kt
@@ -1,2 +1,33 @@
 package com.android.gatherly.ui.badge
 
+import androidx.lifecycle.ViewModel
+import com.android.gatherly.model.profile.ProfileRepository
+import com.android.gatherly.ui.events.UIState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+data class UIState(
+    val badgeTodoCreated : Triple<String, String, String> = Triple("", "", ""),
+    val badgeTodoCompleted: Triple<String, String, String> = Triple("", "", ""),
+    val badgeEventCreated : Triple<String, String, String> = Triple("", "", ""),
+    val badgeEventCompleted: Triple<String, String, String> = Triple("", "", ""),
+    val badgeFriendAdded : Triple<String, String, String> = Triple("", "", ""),
+    val badgeFocusSessionCompleted : Triple<String, String, String> = Triple("", "", ""),
+)
+
+class BadgeViewModel(
+    private val repository: ProfileRepository
+) : ViewModel() {
+
+    private val _uiState: MutableStateFlow<UIState> = MutableStateFlow(UIState())
+
+    /**
+     * StateFlow exposing the current UI state, including all event lists categorized by user
+     * relationship.
+     */
+    val uiState: StateFlow<UIState> = _uiState.asStateFlow()
+
+
+
+}
