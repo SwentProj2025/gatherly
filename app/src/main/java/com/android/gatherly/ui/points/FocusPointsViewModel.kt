67dc170a5cfc708581a7a2b4bd7f9cc5e81f282b
gkerjan <gersende.kerjan@epfl.ch>
2025-11-25 16:03:25 +0100

    feat(points): implement focus points VM

---
+++ app/src/main/java/com/android/gatherly/ui/points/FocusPointsViewModel.kt
@@ -0,0 +1,34 @@
+package com.android.gatherly.ui.points
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.gatherly.model.points.Points
+import com.android.gatherly.model.points.PointsRepository
+import com.android.gatherly.model.points.PointsRepositoryFirestore
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+data class FocusPointsUIState(val focusHistory: List<Points> = emptyList())
+
+class FocusPointsViewModel(
+    private val pointsRepository: PointsRepository = PointsRepositoryFirestore(Firebase.firestore)
+) : ViewModel() {
+
+  private val _uiState = MutableStateFlow(FocusPointsUIState())
+  val uiState: StateFlow<FocusPointsUIState> = _uiState.asStateFlow()
+
+  init {
+    loadPointsHistory()
+  }
+
+  fun loadPointsHistory() {
+    viewModelScope.launch {
+      val userHistory = pointsRepository.getAllPoints()
+      _uiState.value = _uiState.value.copy(focusHistory = userHistory)
+    }
+  }
+}
