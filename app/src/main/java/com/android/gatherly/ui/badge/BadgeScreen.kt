681f0410457b8bd1f6434633d87c8868f34af00b
coco-jrdn <jourdan.colombe@gmail.com>
2025-11-27 23:56:37 +0100

    test(badge): add test and documentation

--- app/src/main/java/com/android/gatherly/ui/badge/BadgeScreen.kt
+++ app/src/main/java/com/android/gatherly/ui/badge/BadgeScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -35,6 +36,24 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 
+/**
+ * Contains test tags used in [BadgeScreen] and its child composables for identifying UI elements
+ * during Compose UI testing.
+ */
+object BadgeScreenTestTags {
+  /** Test tag for the Badge Items */
+  fun badgeTest(title: String): String {
+    return "badge_$title"
+  }
+}
+
+/**
+ * The Badge screen displays a list of highest ranked badges per type that the user obtained
+ *
+ * @param goBack called when the back arrow of the top bar is clicked to go back to Profile Screen
+ * @param navigationActions Handles navigation between different tabs/screens.
+ * @param viewModel The ViewModel managing the state and logic for the Badge screen
+ */
 @Composable
 fun BadgeScreen(
     viewModel: BadgeViewModel =
@@ -45,6 +64,8 @@ fun BadgeScreen(
 ) {
 
   val uiState by viewModel.uiState.collectAsState()
+
+  LaunchedEffect(Unit) { viewModel.refresh() }
 
   Scaffold(
       topBar = {
@@ -73,7 +94,10 @@ fun BadgeItem(badgeUi: BadgeUI) {
           CardDefaults.cardColors(
               containerColor = MaterialTheme.colorScheme.surfaceVariant,
               contentColor = MaterialTheme.colorScheme.onSurfaceVariant),
-      modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)) {
+      modifier =
+          Modifier.fillMaxWidth()
+              .padding(vertical = 4.dp)
+              .testTag(BadgeScreenTestTags.badgeTest(badgeUi.title))) {
         Row(
             modifier = Modifier.fillMaxWidth().padding(12.dp),
             verticalAlignment = Alignment.CenterVertically,
@@ -85,7 +109,6 @@ fun BadgeItem(badgeUi: BadgeUI) {
 
           Spacer(modifier = Modifier.size(12.dp))
 
-          // Title + description on the right
           Column(modifier = Modifier.fillMaxWidth()) {
             Text(
                 text = badgeUi.title,
