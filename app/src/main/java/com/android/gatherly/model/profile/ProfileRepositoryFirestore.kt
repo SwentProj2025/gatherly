fb9a15af45a372816cfa3807f78ba6b996858936
claumnl <116760695+claumnl@users.noreply.github.com>
2025-12-16 21:51:04 +0100

    refactor(profile): remove unused import

--- app/src/main/java/com/android/gatherly/model/profile/ProfileRepositoryFirestore.kt
+++ app/src/main/java/com/android/gatherly/model/profile/ProfileRepositoryFirestore.kt
@@ -12,7 +12,6 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.ktx.app
 import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.ktx.storage
 import java.io.File
 import kotlinx.coroutines.tasks.await
 
